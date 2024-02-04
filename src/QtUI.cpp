0a48bc9b2c41fddf046a5e7fbbe4b93d7bb1dd0c
Jonas Prohaska <e1449302@student.tuwien.ac.at>
2024-02-04 01:33:05 +0100

    .css for styling

--- src/QtUI.cpp
+++ src/QtUI.cpp
@@ -5,6 +5,13 @@ QtUI::QtUI(QWidget* parent) : QMainWindow(parent) {
 	setWindowTitle("My First Qt App");
 	resize(640, 480); // Initial window size (width, height)
 
+
+	QFile main_css("./styles/mainwindow.css");
+	if (main_css.open(QFile::ReadOnly)) {
+		QString style_sheet = QLatin1String(main_css.readAll());
+		qApp->setStyleSheet(style_sheet);
+		main_css.close();
+	}
 	button = new QPushButton("Toggle Fullscreen", this);
 	connect(button, &QPushButton::clicked, this, &QtUI::toggleFullscreen);
 }
